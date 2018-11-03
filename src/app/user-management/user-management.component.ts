68625de3107308944d6e2c7cca7653884c302151
dov118 <kevincarlier118@gmail.com>
2018-11-03 16:45:05 +0100

    Adaptation API OK => UserManagement

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -1,48 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { Observable } from 'rxjs';
 
 import { AppComponent } from '../app.component';
+
+import { Api } from '../Class/Api';
 import { User } from '../Class/User';
+
 import { UserService } from '../Services/user.service';
 
 @Component({
   templateUrl: './user-management.component.html'
 })
 export class UserManagementComponent implements OnInit {
+  private Reponse_getUserById: Observable<Api>;
+  private Reponse_getUserList: Observable<Api>;
+
   public _currentUser: User;
   private UserList: User[];
 
   constructor(private app:AppComponent, private router: Router, private userApi: UserService) {
+    this.Reponse_getUserById = null;
+    this.Reponse_getUserList = null;
+
     this._currentUser = new User(null);
     this.UserList = null;
   }
 
   ngOnInit(): void { 
     this.app.ngOnInit();
-    this._currentUser = this.app._currentUser;
+    this.Reponse_getUserById = this.app.Reponse_getUserById;
 
-    if(!this._currentUser.group.rightGroupPage.UserManagement_Access) {
-      console.log("Vous n'avez pas la permission d'accedez à cette page");
-      this.router.navigate(['/Accueil']);
+    if(this.Reponse_getUserById === null) {
+      this._currentUser = new User(null);
+
+      if(!this._currentUser.group.rightGroupPage.UserManagement_Access) {
+        console.log("Vous n'avez pas la permission d'accedez à cette page");
+        this.router.navigate(['/Accueil']);
+      }
+    } else {
+      this.Reponse_getUserById.subscribe((data: Api) => {
+        this._currentUser = data.data
+
+        if(!data.data.group.rightGroupPage.UserManagement_Access) {
+          console.log("Vous n'avez pas la permission d'accedez à cette page");
+          this.router.navigate(['/Accueil']);
+        }
+      })
     }
     this.getUserList();
   }
 
   private getUserList(): void {
-    //this.UserList = this.userApi.getUserList();
-    var i: number = 0;
-    for(var user of this.UserList) {
-      var pass: string = "";
-      var j: number = 0;
-      while(j < user.password.length) {
-        pass += "*";
-        j++;
+    this.Reponse_getUserList = this.userApi.getUserList();
+    this.Reponse_getUserList.subscribe((data: Api) => {
+      this.UserList = data.data
+
+      for(var user of this.UserList) {
+        if(user.group.rightGroupPage.name.split('_')[1] === "user")
+          user.group.name = "Groupe personnel de droit de " + user.login;
       }
-      this.UserList[i].password = pass;
-      i++
-      if(user.group.rightGroupPage.name.split('_')[1] === "user") {
-        user.group.name = "Groupe personnel de droit de " + user.login;
-      }
-    }
+    })
   }
 }
