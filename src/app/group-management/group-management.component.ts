70a7e54e9ba64111efe1078e5ed232c2e8cffb7d
dov118 <kevincarlier118@gmail.com>
2018-11-03 16:57:29 +0100

    Adaptation API OK=> GroupeManagement

--- src/app/group-management/group-management.component.ts
+++ src/app/group-management/group-management.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { Observable } from 'rxjs';
 
 import { AppComponent } from '../app.component';
 
+import { Api } from '../Class/Api';
 import { User } from '../Class/User'; 
 import { Group } from '../Class/Group';
 
@@ -13,11 +15,19 @@ import { UserService } from '../Services/user.service';
   templateUrl: './group-management.component.html'
 })
 export class GroupManagementComponent implements OnInit {
+  private Reponse_getUserById: Observable<Api>;
+  private Reponse_getUserList: Observable<Api>;
+  private Reponse_getGroupList: Observable<Api>;
+
   public _currentUser: User;
   private GroupList: Group[];
   private UserList: User[];
 
   constructor(private app:AppComponent, private router: Router, private groupApi: GroupService, private userApi: UserService) {
+    this.Reponse_getUserById = null;
+    this.Reponse_getUserList = null;
+    this.Reponse_getGroupList = null;
+
     this._currentUser = new User(null);
     this.GroupList = []
     this.UserList = null;
@@ -25,31 +35,45 @@ export class GroupManagementComponent implements OnInit {
 
   ngOnInit(): void { 
     this.app.ngOnInit();
-    this._currentUser = this.app._currentUser;
+    this.Reponse_getUserById = this.app.Reponse_getUserById;
 
-    if(!this._currentUser.group.rightGroupPage.GroupManagement_Access) {
-      console.log("Vous n'avez pas la permission d'accedez à cette page");
-      this.router.navigate(['/Accueil']);
+    if(this.Reponse_getUserById === null) {
+      this._currentUser = new User(null);
+
+      if(!this._currentUser.group.rightGroupPage.GroupManagement_Access) {
+        console.log("Vous n'avez pas la permission d'accedez à cette page");
+        this.router.navigate(['/Accueil']);
+      }
+    } else {
+      this.Reponse_getUserById.subscribe((data: Api) => {
+        this._currentUser = data.data
+
+        if(!data.data.group.rightGroupPage.GroupManagement_Access) {
+          console.log("Vous n'avez pas la permission d'accedez à cette page");
+          this.router.navigate(['/Accueil']);
+        }
+      })
     }
 
-    this.getUserList();
-    
-    // for(let group of this.getGroupList()) {
-    //   if(group.name.split('_')[1] === "user") {
-    //     group.name = "Groupe personnel de droit de " + this.UserList[this.UserList.findIndex(d => d.id === Number(group.name.split('_')[2]))].login;
-    //     group.rightGroupPage.name = group.name
-    //     this.GroupList.push(group);
-    //   }
-    //   else
-    //     this.GroupList.push(group);
-    // }
+    this.getGroupList();
   }
 
-  // private getGroupList(): Group[] {
-  //   return this.groupApi.getGroupList();
-  // }
+  private getGroupList(): void {
+    this.Reponse_getUserList = this.userApi.getUserList();
+    this.Reponse_getUserList.subscribe((data: Api) => {
+      this.UserList = data.data
+    })
 
-  private getUserList(): void {
-    //this.UserList = this.userApi.getUserList();
+    this.Reponse_getGroupList = this.groupApi.getGroupList();
+    this.Reponse_getGroupList.subscribe((data: Api) => {
+      this.GroupList = data.data
+
+      for(let group of data.data) {
+        if(group.name.split('_')[1] === "user") {
+          group.name = "Groupe personnel de droit de " + this.UserList[this.UserList.findIndex(d => d.id === Number(group.name.split('_')[2]))].login;
+          group.rightGroupPage.name = group.name
+        }
+      }
+    })
   }
 }
